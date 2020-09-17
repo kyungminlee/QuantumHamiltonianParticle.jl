d32ebbc4192c19d2cd97b96ee3a8ac0db0b2b0d2
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-17 17:53:06 -0400

    Fix isparityodd

--- src/util.jl
+++ src/util.jl
@@ -16,7 +16,7 @@ export isparityodd
 # in utility
 function isparityodd(arr::AbstractVector{T})::Bool where T
     n = length(arr)
-    parity = true
+    parity = false
     for i in n-1:-1:1, j in i+1:n
         if arr[j] < arr[i]
             parity = !parity
