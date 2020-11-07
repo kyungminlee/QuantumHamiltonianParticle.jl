a97aea200d61b7b1a09571cab2ab8dad78789a19
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-07 16:53:19 -0500

    Fix ParticleLadderNull
    exchangesign and constructor

--- src/ladder/ladder_null.jl
+++ src/ladder/ladder_null.jl
@@ -2,12 +2,14 @@ export ParticleLadderNull
 import LinearAlgebra
 
 struct ParticleLadderNull{PS<:ParticleSector} <:AbstractParticleLadder{PS, Int}
-    ParticleLadderNull(::Type{PS}) where {PS} = new{PS}()
     ParticleLadderNull{PS}() where {PS} = new{PS}()
+    ParticleLadderNull(::Type{PS}) where {PS} = new{PS}()
+    ParticleLadderNull(::PS) where {PS} = new{PS}()
 end
 
-exchangesign(::ParticleLadderNull, ::AbstractParticleLadder) = 1
-exchangesign(::AbstractParticleLadder, ::ParticleLadderNull) = 1
+exchangesign(::ParticleLadderNull{PS}, ::ParticleLadderNull{PS}) where {PS} = 1
+exchangesign(::ParticleLadderNull{PS}, ::AbstractParticleLadder{PS, S}) where {PS, S} = 1
+exchangesign(::AbstractParticleLadder{PS, S}, ::ParticleLadderNull{PS}) where {PS, S} = 1
 Base.iszero(::ParticleLadderNull) = true
 Base.adjoint(x::ParticleLadderNull) = x
 LinearAlgebra.ishermitian(::ParticleLadderNull) = true
