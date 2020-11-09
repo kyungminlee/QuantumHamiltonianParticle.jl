55c15531fa024f1c80ad06e505a9b332723f9c57
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-08 20:06:34 -0500

    Add test_ladder_embed.jl (empty)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ include("test_hilbert.jl")
 include("test_repconv.jl")
 include("test_ladder.jl")
 include("test_ladder_iterator.jl")
+include("test_ladder_embed.jl")
+
 
 include("test_symmetry_apply.jl")
 include("test_hubbard.jl")
---
+++ test/test_ladder_embed.jl
@@ -0,0 +1,27 @@
+using Test
+using ExactDiagonalization
+using Particle
+
+
+@testset "ParticleLadderEmbedding" begin
+    @testset "Constructor" begin
+    end
+
+    @testset "Methods" begin
+    end
+
+    @testset "Iterator" begin
+    end
+
+    @testset "Unary Operation" begin
+    end
+
+    @testset "Binary Operation" begin
+    end
+
+    @testset "apply!" begin
+    end
+
+    @testset "simplify" begin
+    end
+end
