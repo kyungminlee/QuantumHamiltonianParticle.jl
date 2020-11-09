6dd33daf836684c0fe8ebb7a1b755b0c585f51c2
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-08 23:59:42 -0500

    Delete apply! to bra. May lead to inconsistency

--- src/ladder/ladder_embed.jl
+++ src/ladder/ladder_embed.jl
@@ -9,6 +9,8 @@ import ExactDiagonalization.get_element
 import ExactDiagonalization.get_space
 export get_ladder
 
+import ExactDiagonalization.simplify
+
 
 struct ParticleLadderOperatorEmbedding{
     HilbertSpaceType<:Union{<:ParticleHilbertSpace,<:HilbertSpaceSector{<:ParticleHilbertSpace, <:Any}},
@@ -37,8 +39,6 @@ struct ParticleLadderOperatorEmbedding{
     end
 end
 
-get_space(arg::ParticleLadderOperatorEmbedding) = basespace(arg.hilbert_space)
-get_ladder(arg::ParticleLadderOperatorEmbedding) = arg.ladder
 
 function embed(
     hilbert_space::ParticleHilbertSpace{PS, BR, QN},
@@ -46,12 +46,20 @@ function embed(
 ) where {PS, BR, QN, S}
     return ParticleLadderOperatorEmbedding(hilbert_space, ladder)
 end
+
+
 function embed(
     hilbert_space::HilbertSpaceSector{ParticleHilbertSpace{PS, BR, QN}, QN},
     ladder::AbstractParticleLadder{PS, S},
 ) where {PS, BR, QN, S}
     return ParticleLadderOperatorEmbedding(hilbert_space, ladder)
 end
+
+get_space(arg::ParticleLadderOperatorEmbedding) = basespace(arg.hilbert_space)
+get_ladder(arg::ParticleLadderOperatorEmbedding) = arg.ladder
+
+
+# Iterators
 
 function get_row_iterator(wrap::ParticleLadderOperatorEmbedding, args...; kwargs...)
     return get_row_iterator(wrap.hilbert_space, wrap.ladder, args...; kwargs...)
@@ -65,7 +73,9 @@ function get_element(wrap::ParticleLadderOperatorEmbedding, args...; kwargs...)
     return get_element(wrap.hilbert_space, wrap.ladder, args...; kwargs...)
 end
 
+
 # Equality
+
 function Base.:(==)(lhs::O, rhs::O) where {O<:ParticleLadderOperatorEmbedding}
     return (get_space(lhs) == get_space(rhs)) && (lhs.ladder == rhs.ladder)
 end
@@ -91,7 +101,6 @@ end
 # Unary Operations
 
 Base.iszero(arg::ParticleLadderOperatorEmbedding) = Base.iszero(get_ladder(arg))
-
 Base.:(+)(arg::ParticleLadderOperatorEmbedding) = arg
 Base.:(-)(arg::ParticleLadderOperatorEmbedding) = ParticleLadderOperatorEmbedding(arg.hilbert_space, -arg.ladder_oerator)
 Base.adjoint(arg::ParticleLadderOperatorEmbedding) = ParticleLadderOperatorEmbedding(arg.hilbert_space, Base.adjoint(arg.ladder))
@@ -131,6 +140,8 @@ for binop in [:*, :\]
 end
 
 
+# apply!
+
 function apply!(
     out::SparseState{S1, BR},
     op::ParticleLadderOperatorEmbedding{H, L, P, S3},
@@ -146,22 +157,7 @@ function apply!(
 end
 
 
-function apply!(
-    out::SparseState{S1, BR},
-    state::SparseState{S2, BR},
-    op::ParticleLadderOperatorEmbedding{H, L, P, S3},
-) where {BR, S1, S2, H, L, P, S3}
-    for (bvec, ampl) in state.components
-        for (newbvec, ampl_op) in get_row_iterator(get_space(op), get_ladder(op), bvec)
-            a = get(out.components, newbvec, zero(S2))
-            out[newbvec] = a + ampl * ampl_op
-        end
-    end
-    return out
-end
-
-
-import ExactDiagonalization.simplify
+# simplify
 
 function simplify(arg::ParticleLadderOperatorEmbedding)
     return ParticleLadderOperatorEmbedding(arg.hilbert_space, simplify(arg.ladder))
