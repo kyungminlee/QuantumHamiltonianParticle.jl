dbbfb4a6030ddca8b21db01e990f3a7a44f40660
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-10 11:12:43 -0500

    Update test_proj.jl

--- test/test_proj.jl
+++ test/test_proj.jl
@@ -7,12 +7,109 @@ using Random
 
 @testset "Particle Projector Operators" begin
     @testset "ParticleProjectorUnitOperator" begin
-        p = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0000, 0b0010, 1.0) # should work fine
+        pi = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1) # should work fine
+        pd = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1.0) # should work fine
+        pz = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1.0 + 0.0im) # should work fine
         @test_throws ArgumentError ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0000, 0b0011, 1.0) # parity bit overlap
         @test_throws ArgumentError ParticleProjectorUnitOperator(0b0101, 0b0110, 0b0000, 0b0000, 1.0) # row
         @test_throws ArgumentError ParticleProjectorUnitOperator(0b0101, 0b0100, 0b1000, 0b0000, 1.0) # row
 
-        zero(p)
+        @test pi == ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1)
+        @test pi != ParticleProjectorUnitOperator(0b1101, 0b0100, 0b0001, 0b0010, 1)
+        @test pi != ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0001, 0b0010, 1)
+        @test pi != ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0000, 0b0010, 1)
+        @test pi != ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0000, 1)
+        @test pi != ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 2)
+        @test isapprox(pi, ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1))
+        @test isapprox(pd, ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1))
+        @test isapprox(pd, ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1.0))
+        @test isapprox(pd, ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1.0 + 1E-12))
+        @test isapprox(pd, ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 1.0 + 1E-12im))
+
+        x = zero(pi)
+        @test x.bitmask == 0x0
+        @test x.bitrow == 0x0
+        @test x.bitcol == 0x0
+        @test x.parity_bitmask == 0x0
+        @test iszero(x.amplitude)
+        @test typeof(x.amplitude) == Int
+
+        x = zero(pd)
+        @test x.bitmask == 0x0
+        @test x.bitrow == 0x0
+        @test x.bitcol == 0x0
+        @test x.parity_bitmask == 0x0
+        @test iszero(x.amplitude)
+        @test typeof(x.amplitude) == Float64
+
+        x = zero(pz)
+        @test x.bitmask == 0x0
+        @test x.bitrow == 0x0
+        @test x.bitcol == 0x0
+        @test x.parity_bitmask == 0x0
+        @test iszero(x.amplitude)
+        @test typeof(x.amplitude) == ComplexF64
+
+        x = one(pi)
+        @test x.bitmask == 0x0
+        @test x.bitrow == 0x0
+        @test x.bitcol == 0x0
+        @test x.parity_bitmask == 0x0
+        @test isone(x.amplitude)
+        @test typeof(x.amplitude) == Int
+
+        x = one(pd)
+        @test x.bitmask == 0x0
+        @test x.bitrow == 0x0
+        @test x.bitcol == 0x0
+        @test x.parity_bitmask == 0x0
+        @test isone(x.amplitude)
+        @test typeof(x.amplitude) == Float64
+
+        x = one(pz)
+        @test x.bitmask == 0x0
+        @test x.bitrow == 0x0
+        @test x.bitcol == 0x0
+        @test x.parity_bitmask == 0x0
+        @test isone(x.amplitude)
+        @test typeof(x.amplitude) == ComplexF64
+    end
+
+    @testset "multiplication" begin
+        @testset "mismatch" begin
+            p1 = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 2)
+            p2 = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 3)
+            @test iszero(p1 * p2)
+        end
+        @testset "match" begin
+            # fermion parity noflip
+            p1 = ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 3)
+            p2 = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0000, 2)
+            @test p1 * p2 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0001, 0b0010, 6)
+
+            # fermion parity reflip
+            p3 = ParticleProjectorUnitOperator(0b0101, 0b0100, 0b0001, 0b0010, 2)
+            @test p1 * p3 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0001, 0b0000, 6)
+        end
+    end
+
+    @testset "scale" begin
+        p1 = ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 3)
+        @test p1 * 4 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 12)
+        @test 4 * p1 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 12)
+        @test p1 / 4 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 0.75)
+        @test p1 // 4 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 3//4)
+        @test 4 \ p1 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 0.75)
+        @test -p1 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, -3)
+        @test +p1 == ParticleProjectorUnitOperator(0b0101, 0b0000, 0b0100, 0b0010, 3)
+        n = 2
+        @test p1^n == p1 * p1
+        @test iszero(p1^n)
+
+        p2 = ParticleProjectorUnitOperator(0b0101, 0b0101, 0b0101, 0b0010, 3)
+        n = 6
+        @test p2^n == p2 * p2 * p2 * p2 * p2 * p2
+        @test !iszero(p2^6)
     end
 end
 
