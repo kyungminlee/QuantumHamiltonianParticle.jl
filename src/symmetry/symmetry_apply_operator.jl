be5a7c1f4235d348986329fe85b1bd0f5bd87a08
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-11 09:50:26 -0500

    Fix symmetry_apply for ladder with hilbert space

--- src/symmetry/symmetry_apply_operator.jl
+++ src/symmetry/symmetry_apply_operator.jl
@@ -3,15 +3,15 @@ using LatticeTools
 # import ExactDiagonalization.AbstractSymmetryOperation
 import ExactDiagonalization.symmetry_apply
 
-# === Ladder Unit ===
-
 function symmetry_apply(
-    hs::ParticleHilbertSpace,
+    ::ParticleHilbertSpace{PS, BR, QN},
     permutation::SitePermutation,
-    op::ParticleLadderUnit{PS, <:Integer, <:Integer},
-) where PS
-    return ParticleLadderUnit(PS, op.particle_index, permutation(op.orbital), op.ladder)
+    op::AbstractParticleLadder{PS},
+) where {PS, BR, QN}
+    return symmetry_apply(permutation, op)
 end
+
+# === Ladder Unit ===
 
 function symmetry_apply(
     permutation::SitePermutation,
@@ -21,38 +21,15 @@ function symmetry_apply(
 end
 
 # === Ladder Product ===
+
 # No need to worry about signs here. The operators are not ordered.
-
-function symmetry_apply(
-    hs::ParticleHilbertSpace,
-    permutation::SitePermutation,
-    op::ParticleLadderProduct{PI, OI},
-) where {PI, OI}
-    return ParticleLadderProduct([symmetry_apply(hs, permutation, f) for f in op.factors])
-end
-
-function symmetry_apply(
-    permutation::SitePermutation,
-    op::ParticleLadderProduct{PI, OI},
-) where {PI, OI}
+function symmetry_apply(permutation::SitePermutation, op::ParticleLadderProduct{PI, OI}) where {PI, OI}
     return ParticleLadderProduct([symmetry_apply(permutation, f) for f in op.factors])
 end
 
 # === Ladder Sum ===
 
-function symmetry_apply(
-    hs::ParticleHilbertSpace,
-    permutation::SitePermutation,
-    op::ParticleLadderSum{PI, OI},
-) where {PI, OI}
-    return ParticleLadderSum([symmetry_apply(hs, permutation, t) => a for (t, a) in op.terms])
-end
-
-
-function symmetry_apply(
-    permutation::SitePermutation,
-    op::ParticleLadderSum{PI, OI},
-) where {PI, OI}
+function symmetry_apply(permutation::SitePermutation, op::ParticleLadderSum{PI, OI}) where {PI, OI}
     return ParticleLadderSum([symmetry_apply(permutation, t) => a for (t, a) in op.terms])
 end
 
@@ -64,16 +41,10 @@ end
 
 import ExactDiagonalization.isinvariant
 
-function isinvariant(
-    permutation::SitePermutation,
-    op::AbstractParticleOperator,
-)
+function isinvariant(permutation::SitePermutation, op::AbstractParticleOperator)
     return iszero(simplify(symmetry_apply(permutation, op) - op))
 end
 
-function isinvariant(
-    permutation::SitePermutation,
-    op::AbstractParticleLadder,
-)
+function isinvariant(permutation::SitePermutation, op::AbstractParticleLadder)
     return iszero(simplify(symmetry_apply(permutation, op) - op))
 end
