8c276cdfb0425e8b640a4cb54180e666ca4f12f2
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-11 08:38:34 -0500

    Add tests for embedded projector

--- test/test_proj.jl
+++ test/test_proj.jl
@@ -247,9 +247,34 @@ end
     hilbert_space = ParticleHilbertSpace([site, site, site])
     hsr = represent(hilbert_space)
     rng = MersenneTwister(0)
-    for lad in [c(1,2), cdag(1,2), c(2,2), cdag(2,2), c(3,2), cdag(3,2), cdag(1,2)*c(2,1), cdag(1,2)*c(2,1) + cdag(2,2)*0.3]
+    for lad in [c(1,2), cdag(1,2), c(2,2), cdag(2,2), c(3,2), cdag(3,2),
+                cdag(1,2)*c(2,1), cdag(1,2)*c(2,1) + cdag(2,2)*0.3]
         opa = embed(hilbert_space, lad)
         opb = make_projector_operator(hilbert_space, lad)
+        opc = make_projector_operator(opa)
+        for bvec in rand(rng, hsr.basis_list, 5)
+            out1a = Dict(collect(get_column_iterator(opa, bvec)))
+            out1b = Dict(collect(get_column_iterator(opb, bvec)))
+            out1c = Dict(collect(get_column_iterator(opc, bvec)))
+            @test out1a == out1b == out1c
+            out2a = Dict(collect(get_row_iterator(opa, bvec)))
+            out2b = Dict(collect(get_row_iterator(opb, bvec)))
+            out2c = Dict(collect(get_row_iterator(opc, bvec)))
+            @test out2a == out2b == out2c
+
+            state = SparseState(bvec=>1)
+            out3a = SparseState{Float64, UInt}()
+            out3b = SparseState{Float64, UInt}()
+            out3c = SparseState{Float64, UInt}()
+            apply!(out3a, opa, state)
+            apply!(out3b, opb, state)
+            apply!(out3c, opc, state)
+            @test out3a == out3b == out3c
+        end
+
+        hilbert_space_sector = HilbertSpaceSector(hilbert_space, (2,0))
+        opa = embed(hilbert_space_sector, lad)
+        opb = make_projector_operator(hilbert_space_sector, lad)
         opc = make_projector_operator(opa)
         for bvec in rand(rng, hsr.basis_list, 5)
             out1a = Dict(collect(get_column_iterator(opa, bvec)))
